c96dc444b7abf1cffa069316e71d6212622a490e
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-10-12 11:16:06 -0500

    feat: :sparkles: audit when a user make a requests of charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Services\DebtService;
 use App\Services\TransactionService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function __construct(protected TransactionService $transactionService, protected DebtService $debtService) {}
+    public function __construct(protected TransactionService $transactionService, protected DebtService $debtService, protected UserService $userService) {}
 
     public function index(Request $request)
     {
@@ -19,6 +20,9 @@ class DashboardController extends Controller
         $toDebts = $request->input('toDebts');
         $limitDebts = $request->input('limitDebts', 10);
         $data = $this->debtService->getTopContactsWithPendingDebts($limitDebts, $fromDebts, $toDebts);
+        activity()
+            ->causedBy($this->userService->getUserLoggedIn())
+            ->log('Consultó gráficos de estadísticas');
         return view('dashboard.dashboard', compact('chartData','fromDebts','toDebts','data','fromDebts','toDebts','limitDebts'));
     }
 }
