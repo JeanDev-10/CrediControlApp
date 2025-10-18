8dd87e44d9741a04ee722827d0b22da6342bd582
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-10-17 23:11:29 -0500

    feat: :sparkles: roles admin and client add to app
    ready migrations and seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,18 +16,33 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $admin_role = Role::create(['name' => 'admin']);
+        $client_role = Role::create(['name' => 'client']);
+        Permission::firstOrCreate(['name' => 'user.create']);
+        Permission::firstOrCreate(['name' => 'user.show']);
+        Permission::firstOrCreate(['name' => 'user.update']);
+        Permission::firstOrCreate(['name' => 'user.index']);
+        Permission::firstOrCreate(['name' => 'user.toogleIsActive']);
+        Permission::firstOrCreate(['name' => 'user.exportToPdf']);
+        $admin = User::create([
+            'name' => 'Jean Pierre',
+            'lastname' => 'Rodríguez Zambrano',
+            'email' => 'admin@hotmail.com',
+            'password' => bcrypt('Jean1234@.'),
+        ]);
+        $admin->assignRole('admin');
+        $admin_role->syncPermissions(Permission::all());
         User::create([
             'name' => 'Jean Pierre',
-            "lastname" => "Rodríguez",
-            'email' => "jean@hotmail.com",
-            "password"=>bcrypt('jean1234'),
-        ]);
+            'lastname' => 'Rodríguez',
+            'email' => 'jean@hotmail.com',
+            'password' => bcrypt('jean1234'),
+        ])->assignRole($client_role);
         User::create([
             'name' => 'test user 2',
-            "lastname" => "apellido",
-            'email' => "test@hotmail.com",
-           "password"=>bcrypt('password1234'),
-        ]);
+            'lastname' => 'apellido',
+            'email' => 'test@hotmail.com',
+            'password' => bcrypt('password1234'),
+        ])->assignRole($client_role);
     }
 }
